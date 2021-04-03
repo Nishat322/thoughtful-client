0f57eaa5948e17d3bc2d09bbeeb8097d1b2f07d4
Nishat Hoque <hoquen0397@gmail.com>
2021-04-03 18:52:12 -0400

    css changes

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
-//props.description => description of the project
-//button sign up will lead to registration page
+import './LandingPage.css'
 class LandingPage extends Component {
     render() { 
         return (  
             <div className = 'LandingPage'>
-                 <h1><Link to = '/thoughts'> Thoughtful Thoughts </Link> </h1>
-                <h2>My Capstone Project</h2>
-                    <h3> Paleo hexagon marfa vegan actually small batch. Synth glossier shaman normcore tilde, taiyaki cloud bread blog pickled. Franzen portland banh mi tilde, aesthetic artisan hashtag forage synth 3 wolf moon messenger bag actually. Roof party gochujang narwhal blue bottle direct trade cardigan iPhone health goth art party lumbersexual lyft seitan tote bag heirloom you probably haven't heard of them. </h3>
+                <h1>Welcome to Thoughtful Thoughts</h1>
+                    <h3> This application has been build to collect your thoughts. Simple one-liners about anything. You can either add of delete </h3>
                     <h3> Cronut scenester selfies palo santo meh kale chips. Cloud bread hashtag occupy ethical, chia gastropub jianbing shaman semiotics deep v cornhole plaid. Pinterest fashion axe sriracha deep v. Microdosing keytar pabst, heirloom yr art party pitchfork hella whatever put a bird on it migas man braid fixie. Chicharrones mumblecore before they sold out, pinterest master cleanse you probably haven't heard of them tattooed tumblr typewriter sriracha. Single-origin coffee marfa offal put a bird on it. Jean shorts selvage migas leggings gochujang.</h3>
+                <div className = 'LandingPage_enter'>
+                    <h2><Link to = '/thoughts'> Enter to Dump Your Thoughts </Link> </h2>
+                </div>
             </div>
         )
     }
